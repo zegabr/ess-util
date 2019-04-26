6e9205a8c7e2aa3f93b606281203dfce4c3b312b
Jose Gabriel Silva Pereira <jgsp2@ug3c30.windows.cin.ufpe.br>
2019-04-26 08:38:23 -0300

    Atualizado para instalar o protractor e ser reutilizável no mesmo PC

--- README.md
+++ README.md
@@ -2,9 +2,12 @@
 scripts úteis para a disciplina de Engenharia de Software e Sistemas
 
 ## conteúdo
-e.sh -> instala angular e typescript num diretório chamado ESS no Desktop. Recomenda-se rodar esse script somente uma vez por PC do grad. 
+e.sh -> instala angular, typescript e protractor num diretório chamado ESS no Desktop.
 
-(O script coloca os caminhos de angular e typescript instalados na variável PATH através da edição do arquivo ~/.bashrc)
+(O script coloca os caminhos de angular, typescript e protractor instalados na variável PATH através da edição do arquivo ~/.bashrc)
+
+ps: Atualizado para ser possível rodar mais de uma vez num mesmo linux do cin (ele apaga possíveis instalacoes na pasta root e possíveis usos anteriores desse script)
+
 
 ## como rodar
 Abra o terminal e digite `source e.sh` e o que foi descrito acontecerá.
--- e.sh
+++ e.sh
@@ -1,17 +1,30 @@
 #e de ESS
+
+#cat bashreserva > ~/.bashrc #reseta bashrc do linux caso rodado diretamente da pasta ess-util
+#cat ess-util/bashreserva > ~/.bashrc #reseta bashrc do linux caso rodado do diretorio no qual ess-util foi clonado
+#source ~/.bashrc
+
+rm -rf ~/node_modules/ #remove possiveis instalacoes antigas na root
+
 cd ~/Desktop
 mkdir ESS
 cd ESS
+rm -rf node_modules/ ##remove possiveis usos anteriores deste script
 
+
+##reinstala tudo
 npm install @angular/cli
 npm install typescript
+npm install protractor
 
-
-cd node_modules/@angular/cli/bin
+cd ~/Desktop/ESS/node_modules/@angular/cli/bin
 pwd > ~/path1
 
 cd ~/Desktop/ESS/node_modules/typescript/bin
 pwd > ~/path2
+
+cd ~/Desktop/ESS/node_modules/protractor/bin
+pwd > ~/path3
 
 
 printf "
@@ -22,7 +35,11 @@ printf "
 PATH=\$PATH:" >> ~/.bashrc
 cat ~/path2 >> ~/.bashrc
 
+printf "
+PATH=\$PATH:" >> ~/.bashrc
+cat ~/path3 >> ~/.bashrc
+
 source ~/.bashrc
 
-rm ~/path1 ~/path2
+rm ~/path1 ~/path2 ~/path3
 
